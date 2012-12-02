b734dcf46a76ab20264c1bcb01422090055f923b
Christian Vuerings <vueringschristian@gmail.com>
2012-12-01 21:31:26 -0800

    Update the testmail2 page

--- testmail2.php
+++ testmail2.php
@@ -1,6 +1,10 @@
+<?php
+
 $to = "vueringschristian@gmail.com";
 $subject = "ZDNet Developer article";
 $msg = "I completely understand SMTP servers now!";
 $headers = "From: info@sintblasiushof.be\nReply-To: info@sintblasiushof.be";
 mail("$to", "$subject", "$msg", "$headers");
 echo "finished!";
+
+?>
